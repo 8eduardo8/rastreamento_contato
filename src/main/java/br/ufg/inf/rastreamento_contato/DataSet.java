c6ea25dbfdc54161cd202931e7f4f605c90c04be
8eduardo8 <carlos_rosa8@hotmail.com>
2020-07-08 23:20:57 -0300

    criando dataset

--- src/main/java/br/ufg/inf/rastreamento_contato/DataSet.java
+++ src/main/java/br/ufg/inf/rastreamento_contato/DataSet.java
@@ -1,11 +1,41 @@
 package br.ufg.inf.rastreamento_contato;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataSet {
 
-	public static void main(String[] args) {
+	private static final int linhas = 50, colunas = 50;
+	private static final int qtdeAgentes = 10;
+	private static final double probabilidadeMover = 0.2;
+	private static final int tempos = 100;
 
+	private static final int TIPOMOVIMENTO_ALEATORIO = 1;
+	private static final int TIPOMOVIMENTO_DIRECIONAL = 2;
+
+	private static final int tipoMovimento = TIPOMOVIMENTO_DIRECIONAL;
+
+	public static void main(String[] args) {
 		System.out.println("Criando DatSet");
+
+		List<Agente> agentes = new ArrayList();
+
+		for (int i = 0; i < qtdeAgentes; i++) {
+			agentes.add(new Agente());
+		}
 
 	}
 
+	public static class Agente {
+		int id;
+		public List<Movimento> movimentos;
+
+		public Agente() {
+		}
+	}
+
+	public static class Movimento {
+		int tempo;
+		int posX = 0, posY = 0;
+	}
 }
