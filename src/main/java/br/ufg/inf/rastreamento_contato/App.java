2a8d2eea0c8a429b4ef9cdc84a91c86670c16859
8eduardo8 <carlos_rosa8@hotmail.com>
2020-07-08 22:58:29 -0300

    Initial commit

---
+++ src/main/java/br/ufg/inf/rastreamento_contato/App.java
@@ -0,0 +1,13 @@
+package br.ufg.inf.rastreamento_contato;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
